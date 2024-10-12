b4d9e7139b1e11e06b970006ddb6d435485ed69a
feganflamer035 <usejohnny@gmail.com>
2024-10-12 17:44:47 -0500

    2024-10-12 intial commit

---
+++ run_logikprojekt_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/asap-p620/workspace/GitHub/LOGIK-PROJEKT-JFEGAN
+/opt/Autodesk/python/2025.2.pr208/bin/python /home/asap-p620/workspace/GitHub/LOGIK-PROJEKT-JFEGAN/logik_projekt.py
